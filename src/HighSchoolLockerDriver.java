d861ed7c198bb6b1e9cb4e3d2d3e76ffe4873513
juanarrivillaga <juanpa.arrivillaga@gmail.com>
2015-02-28 02:59:46 -0500

    sample output commented at end of code

--- src/HighSchoolLockerDriver.java
+++ src/HighSchoolLockerDriver.java
@@ -19,7 +19,7 @@ public class HighSchoolLockerDriver {
 		System.out.println();
 		System.out.println();
 		
-		System.out.println("OK, let's remove a book from Donal Duck's locker");
+		System.out.println("OK, let's remove a book from Donald Duck's locker");
 		secondLocker.removeBookFromLocker();
 		
 		System.out.printf("%n%nIt would probably help to open the locker first...");
@@ -34,3 +34,37 @@ public class HighSchoolLockerDriver {
 	}
 
 }
+/*SAMPE OUTPUT:
+ * Let's try to open  Mickey Mouse's locker with the combination 15,18,18.
+
+The locker won't open!
+
+OK, let's try to right combination: 28,17,39.
+
+The locker opens!
+
+OK, let's add three books to Mickey mouses locker
+A book was added to locker 100
+A book was added to locker 100
+A book was added to locker 100
+
+OK, let's remove a book from Donald Duck's locker
+
+This locker is LOCKED! You can't remove any books.
+
+It would probably help to open the locker first...
+The locker opens!
+
+Ok, now we can remove that book from Donald Duck's locker...
+There are no more books to remove from this locker!
+
+The locker number is: 100
+The locker belongs to: Mickey Mouse
+There is(are) 6 book(s) in the locker.
+The locker is UNLOCKED
+
+The locker number is: 275
+The locker belongs to: Donald Duck
+There is(are) 0 book(s) in the locker.
+The locker is UNLOCKED
+*/
